bf3188ba1573ef08749b1b9f884cf7fcfa12d289
Tan Yuan Ja <tanyuejia05@gmail.com>
2024-04-22 18:14:39 +0800

    first commit

---
+++ comment.py
@@ -0,0 +1,19 @@
+def __init__(self,author,content):
+    self,author=author
+    self,content=content 
+    self,comment=comment 
+
+def add_comment(self,commenter,comment): 
+    self.comment.append(commenter,comment)
+
+def display_comment(self):
+    print("Comment on the post:")
+for commenter, comment in ().comment:
+    print(f"{commenter}:{comment}")
+
+# Create new post
+# Create/Add some comments
+# Display comments under the post 
+# Create vote
+# Create reply 
+# Create share 
