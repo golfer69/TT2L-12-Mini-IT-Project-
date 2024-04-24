4f32bfe417779001214f8f4402be175b7b24e0fa
Tan Yuan Ja <tanyuejia05@gmail.com>
2024-04-24 23:10:32 +0800

    edits

--- comment.py
+++ comment.py
@@ -1,19 +1,35 @@
-def __init__(self,author,content):
-    self,author=author
-    self,content=content 
-    self,comment=comment 
+Post_Comment:
+def __init__(self,text,author):
+    self.text = text 
+    self.author = author
+    self.reply = []
 
-def add_comment(self,commenter,comment): 
-    self.comment.append(commenter,comment)
+def add_reply(self,reply):
+    """Add a reply to this comment.""" 
+    self.reply.append(reply)
 
-def display_comment(self):
-    print("Comment on the post:")
-for commenter, comment in ().comment:
-    print(f"{commenter}:{comment}")
+def display(self,indent=0):
+    """Display the comment and reply."""
+    print(""*indent + f"{self.author}:{self.text}")
+    for reply in self.reply:
+        reply.display(indent + 1)
 
-# Create new post
-# Create/Add some comments
-# Display comments under the post 
+# Example usage in comments:
+top_comment = Post_Comment("This is the top comment.","user1")
+reply1 = Post_Comment("Reply to top comment.", "user2")
+reply2 = Post_Comment("Another reply to top comment.", "user 3")
+reply3 = Post_Comment("A reply to the first reply.", "user4")
+
+top_comment.add.reply(reply1)
+top_comment.add_reply(reply2)
+reply1.add_reply(reply3)
+
+print("Comments:")
+top_comment.display()
+
+
 # Create vote
 # Create reply 
-# Create share 
+# Create share
+# Add timestamps
+# Add user authentication 
