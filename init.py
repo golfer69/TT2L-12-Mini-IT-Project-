15dec13955db0d7f36ce32a116bd6d77f2d2e0ae
golfer69 <145439095+golfer69@users.noreply.github.com>
2024-04-26 17:59:17 +0800

    Update init.py

--- init.py
+++ init.py
@@ -1,17 +1,3 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from database2 import db, User, Posts, web
+import sqlite3
 
-# Routes
-@web.route('/add_data')
-def add_data():
-    print("Adding data...")
-    new_user = User(name='John Doe', email='john@example.com')
-    new_posts= Posts(image='chicken', posts_title='stuff',text='nobody cares')
-    db.session.add(new_user)
-    db.session.add(new_posts)
-    db.session.commit()
-    db.session.close()
-    print('data added successfully')
-    return 'Entry added'
