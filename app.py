4c768548b7a89beb99f1b1fd5a569a097f164383
golfer69 <145439095+golfer69@users.noreply.github.com>
2024-04-29 17:41:23 +0800

    updating some stuff

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from werkzeug.utils import secure_filename
 import os 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, current_user, login_required
+from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -13,6 +14,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'chickenstuffe'
     app.config['UPLOAD_DIRECTORY'] = 'uploads/'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
 
@@ -35,6 +37,12 @@ class User(db.Model, UserMixin):
     # email = db.Column(db.String(100), nullable=False, unique=True)
 
 
+class Text(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255))
+    date_added = db.Column(db.DateTime, default=datetime.now)
+
+
 with app.app_context():
     db.create_all()
 
@@ -54,10 +62,37 @@ class LoginForm(FlaskForm):
     submit= SubmitField('Login')
 
 
-@app.route('/')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file'] 
+    if file:
+        file.save(os.path.join(
+            app.config['UPLOAD_DIRECTORY'],
+            secure_filename(file.filename)
+        ))
+    return redirect('/')
+
+
+@app.route('/serve-files/<filename>', methods=['GET'])
+def serve_files(filename):
+    return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     files = os.listdir(app.config['UPLOAD_DIRECTORY'])
-    return render_template('index.html', files=files)
+    if request.method == 'POST':
+        content = request.form['content']
+        text = Text(content=content)
+        db.session.add(text)
+        db.session.commit()
+        db.session.close()
+        return redirect(url_for('index'))
+    else:
+        texts = Text.query.all()
+        return render_template('index.html', texts=texts, files=files)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -101,27 +136,9 @@ def dashboard():
 
 
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    file = request.files['file'] 
-    if file:
-        file.save(os.path.join(
-            app.config['UPLOAD_DIRECTORY'],
-            secure_filename(file.filename)
-        ))
-    return redirect('/')
-
-
-
-@app.route('/serve-files/<filename>', methods=['GET'])
-def serve_files(filename):
-    return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename)
-
-
 if  __name__ == '__main__':
     app.run(debug=True)
 
-    
 
 
 
