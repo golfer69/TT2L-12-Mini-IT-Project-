c06aeb672f8ed139020ba2832a24c09948bf915a
golfer69 <145439095+golfer69@users.noreply.github.com>
2024-04-29 11:02:45 +0800

    register form created and many other things

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for, flash
 from werkzeug.utils import secure_filename
 import os 
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_login import UserMixin
+# from datetime import datetime
+from flask_login import UserMixin,  LoginManager, login_user, logout_user, current_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -11,6 +11,7 @@ from flask_bcrypt import Bcrypt
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'chickenstuffe'
     app.config['UPLOAD_DIRECTORY'] = 'uploads/'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,11 +21,23 @@ app=create_app()
 db = SQLAlchemy(app) # Initialise database
 bcrypt=Bcrypt(app)
 
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(db.Model, UserMixin):
-    user_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password= db.Column(db.String(40), nullable=False)
-    email = db.Column(db.String(100), nullable=False, unique=True)
+    # email = db.Column(db.String(100), nullable=False, unique=True)
+
+
+with app.app_context():
+    db.create_all()
 
 class RegisterForm(FlaskForm):
     username= StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={'placeholder':'Username'})
@@ -35,56 +48,81 @@ def validate_username(self, username):
     existing_username=User.query.filter_by(username=username.data).first()
     if existing_username:
         raise ValidationError('That username already exists. Please choose another one')
-# class Posts(db.Model):
-#     posts_id=db.Column(db.Integer, primary_key=True)
-#     image=db.Column(db.String(2000))
-#     posts_title=db.Column(db.String(20000), nullable=False)
-#     text=db.Column(db.String(20000), nullable=False)
 
-def create_tables():
-    with app.app_context().push():
-        db.create_all()
+class LoginForm(FlaskForm):
+    username= StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={'placeholder':'Username'})
+    password= StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={'placeholder':'Password'})
+    submit= SubmitField('Login')
 
-app.app_context().push()
 
 @app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_DIRECTORY'])
     return render_template('index.html', files=files)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
     if form.validate_on_submit():
         hashded_password= bcrypt.generate_password_hash(form.password.data)
         new_user= User(username=form.username.data, password=hashded_password)
         db.session.add(new_user)
         db.session.commit()
-        db.session.close()
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Invalid username or password.', 'error')
+        else:
+            flash('Invalid username or password.', 'error')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file'] 
-    
     if file:
         file.save(os.path.join(
             app.config['UPLOAD_DIRECTORY'],
             secure_filename(file.filename)
         ))
-    
     return redirect('/')
+
+
 
 @app.route('/serve-files/<filename>', methods=['GET'])
 def serve_files(filename):
     return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename)
 
+
+
+
 if  __name__ == '__main__':
     app.run(debug=True)
-    
-
-
-
-
-
