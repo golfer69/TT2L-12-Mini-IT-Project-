7748b86336d14ed4628f73c106e12464bcfc515c
Tan Yuan Ja <tanyuejia05@gmail.com>
2024-05-27 03:48:23 +0800

    .

--- tag_user.py
+++ tag_user.py
@@ -16,6 +16,8 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.now)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
