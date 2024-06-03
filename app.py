26550951b635ea7af9fda79cc14f02e4a79d7c91
golfer69 <145439095+golfer69@users.noreply.github.com>
2024-06-03 12:06:13 +0800

    fixing the error

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect, send_from_directory, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for, flash
 from werkzeug.utils import secure_filename
 import os 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField, FileField
+from wtforms import SubmitField, StringField, PasswordField, EmailField, FileField
 from flask_wtf.file import FileAllowed
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
@@ -17,15 +17,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY']='chickenstuffe'
     app.config['UPLOAD_DIRECTORY'] = 'uploads/'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
-    app.config['SQLALCHEMY_BINDS'] = {'data': 'sqlite:///data.db'}
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
 
 app=create_app()
-
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
+
 
 login_manager=LoginManager()
 login_manager.init_app(app)
@@ -36,6 +35,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)  
     username = db.Column(db.String(150), nullable=False, unique=True)
     password= db.Column(db.String(40), nullable=False)
@@ -56,14 +56,12 @@ class Post(db.Model):
     poster_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     community_id = db.Column(db.Integer, db.ForeignKey('community.id'))
     id_for_comments = db.relationship('Comment', backref='text', lazy=True)
-    votes = db.Column(db.Integer, default=0)
-    hidden_votes = db.Column(db.Integer, default=0) # for algorithms
     
 class Comment(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True)
     poster_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))  # Foreign key referencing Text.id
     comment_content = db.Column(db.Text)
 
 class Community(db.Model):
@@ -86,26 +84,31 @@ class Update(db.Model):
     user_id= db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-class Votes(db.Model):
-    __tablename__ = 'votes'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
-    vote_type = db.Column(db.Integer)
+
+
+
 
 # create database
 with app.app_context():
+    # Create Text table first
     db.create_all()
 
 class RegisterForm(FlaskForm):
     username= StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={'placeholder':'Username'})
     password= PasswordField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={'placeholder':'Password'})
+    email= EmailField(validators=[InputRequired(), Length(min=10, max=100)], render_kw={'placeholder':'Email'})
     submit= SubmitField('Register')
 
     def validate_username(self, username):
         existing_username=User.query.filter_by(username=username.data).first()
         if existing_username:
             raise ValidationError('That username already exists. Please choose another one')
+
+    def validate_email(self, email):
+        existing_email=User.query.filter_by(email=email.data).first()
+        if existing_email:
+            raise ValidationError('That email already exists. Please choose another one')
+
 
 class LoginForm(FlaskForm):
     username= StringField(validators=[InputRequired(), Length(min=6, max=25)], render_kw={'placeholder':'Username'})
@@ -120,7 +123,7 @@ class EntryForm(FlaskForm):
     location= StringField(label='Location', validators=[Length(min=1, max=100)])
     interests= StringField(label='Interests', validators=[Length(min=1, max=1000)])    
     faculty= StringField(label='Faculty', validators=[Length(min=1, max=100)])
-    profile_pic=FileField(label='Profile Picture', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    profile_pic=FileField(label='Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit= SubmitField('Submit')
 
 
@@ -135,8 +138,10 @@ def index():
 @app.route('/create', methods=['GET'])
 @login_required
 def create():
+    pics = os.listdir(app.config['UPLOAD_DIRECTORY'])
+    texts = Post.query.all()
     communities = Community.query.all()
-    return render_template('create.html',communities=communities, page_title="Create a post")
+    return render_template('create.html', texts=texts, pics=pics,communities=communities, page_title="Create a post")
 
 @app.route('/createcommunity', methods=['GET'])
 @login_required
@@ -160,6 +165,7 @@ def upload():
                 db.session.commit()
             
                 if file:
+
                     filename = secure_filename(file.filename)
                     file.save(os.path.join(
                         app.config['UPLOAD_DIRECTORY'],
@@ -191,17 +197,24 @@ def upload():
             
     return redirect('/')
 
-@app.route('/create', methods=['GET'])
-@login_required
-def create():
-    communities = Community.query.all()
-    return render_template('create.html',communities=communities, page_title="Create a post")
+@app.route('/update', methods=['GET','POST'])
+def update():
+    post_id = request.form['post_id'] 
+    post = Post.query.get(post_id)
+    if request.method == 'POST' and post.poster_id == current_user.id:
+        
+        new_title = request.form['title']
+        new_content = request.form['content']
 
+        post.title = new_title
+        post.content = new_content
+        
+        db.session.commit()
+    return redirect(url_for('show_post', post_id=post_id))
 
 @app.route('/uploads/<path:filename>')
 def serve_files(filename):
     return send_from_directory(app.config['UPLOAD_DIRECTORY'], filename)
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -210,7 +223,7 @@ def register():
     form = RegisterForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')        
-        new_user= User(username=form.username.data, password=hashed_password)
+        new_user= User(email=form.email.data, username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -327,37 +340,37 @@ def admin():
     if id==1 or id==6:
         return render_template('admin.html', page_title="Admin Page")
     
-@app.route('/delete', methods=['POST'])
-@login_required
-def delete():
-    item = request.form.get('item')
-    if current_user.is_authenticated:
-        if item == "post":
-            post_id = request.form.get('post_id')
-            post = Post.query.get(post_id)
-            if post.poster_id == current_user.id:
-                db.session.delete(post)
-                db.session.commit()
-                # Delete the image file if it exists
-                image_filename = post.image_filename
-                if image_filename:
-                    image_path = os.path.join(app.config['UPLOAD_DIRECTORY'], image_filename)
-                    if os.path.exists(image_path):
-                        try:
-                            os.remove(image_path)
-                        except OSError as e:
-                            print(f"Error deleting image file: {e}")
-            return redirect('/')
-        if item == "comment":
-            comment_id = request.form.get('comment_id')
-            post_id = request.form.get('post_id')
-            comment = Comment.query.get(comment_id)
-            if comment.poster_id == current_user.id:
-                db.session.delete(comment)
-                db.session.commit()
-            return redirect(url_for('show_post', post_id=post_id))
 
-@app.route('/post/<int:post_id>', methods=['GET'])
+
+@app.route('/delete_post/<int:post_id>', methods=['POST'])
+@login_required
+def delete_post(post_id):
+  post = Post.query.get(post_id)
+  poster=current_user.id
+  if post.poster_id==poster:
+    # Delete the post object from the database
+    db.session.delete(post)
+    db.session.commit()
+    
+    # Delete the image file if it exists
+    image_filename = post.image_filename
+    if image_filename:
+      image_path = os.path.join(app.config['UPLOAD_DIRECTORY'], image_filename)
+      if os.path.exists(image_path):
+        try:
+          os.remove(image_path)
+        except OSError as e:
+          print(f"Error deleting image file: {e}")
+          
+  return redirect('/')
+
+@app.route('/edit/<int:post_id>', methods=['GET','POST'])
+def edit_post(post_id):
+    post = Post.query.get(post_id)
+    return render_template('edit.html',post=post)
+
+
+@app.route('/post/<int:post_id>', methods=['GET', 'POST'])
 def show_post(post_id):
     post = Post.query.get(post_id)
     comments = Comment.query.filter_by(post_id=post_id).all()  # Filter comments by post ID
@@ -385,75 +398,7 @@ def show_community(community_name):
 #     time_since_posted = calculate_time_difference(posted_time)
 #     return render_template('post.html', time_since_posted=time_since_posted)
 
-@app.route('/upvote/<int:post_id>', methods=['POST'])
-def upvote(post_id):
-    user_id = current_user.id
-    post = Post.query.get(post_id)
-    if post:
-        existing_vote = Votes.query.filter_by(user_id=user_id, post_id=post_id).first()
-        if existing_vote and existing_vote.vote_type == "downvote":
-            existing_vote.vote_type = "upvote"
-        else:
-            vote = Votes(user_id=user_id, post_id=post_id, vote_type="upvote")
-            db.session.add(vote)
 
-        # Count upvotes and downvotes separately
-        upvote_count = Votes.query.filter_by(post_id=post_id, vote_type="upvote").count()
-        downvote_count = Votes.query.filter_by(post_id=post_id, vote_type="downvote").count()
-
-        post.votes = upvote_count - downvote_count
-        db.session.commit()
-        return jsonify({'message': 'Upvoted successfully', 'votes': post.votes})
-    else:
-        return jsonify({'error': 'Post not found'}), 404
-
-@app.route('/downvote/<int:post_id>', methods=['POST'])
-def downvote(post_id):
-    user_id = current_user.id
-    post = Post.query.get(post_id)
-    if post:
-        existing_vote = Votes.query.filter_by(user_id=user_id, post_id=post_id).first()
-        if existing_vote and existing_vote.vote_type == "upvote":
-            existing_vote.vote_type = "downvote"
-        else:
-            vote = Votes(user_id=user_id, post_id=post_id, vote_type="downvote")
-            db.session.add(vote)
-
-        # Count upvotes and downvotes separately
-        upvote_count = Votes.query.filter_by(post_id=post_id, vote_type="upvote").count()
-        downvote_count = Votes.query.filter_by(post_id=post_id, vote_type="downvote").count()
-
-        post.votes = upvote_count - downvote_count
-        db.session.commit()
-        return jsonify({'message': 'Downvoted successfully', 'votes': post.votes})
-    else:
-        return jsonify({'error': 'Post not found'}), 404
-
-@app.route('/unvote/<int:post_id>', methods=['POST'])
-def unvote(post_id):
-    user_id = current_user.id
-    post = Post.query.get(post_id)
-    if post:
-        existing_vote = Votes.query.filter_by(user_id=user_id, post_id=post_id).first()
-        if existing_vote:
-            db.session.delete(existing_vote)
-            # Count upvotes and downvotes separately
-            upvote_count = Votes.query.filter_by(post_id=post_id, vote_type="upvote").count()
-            downvote_count = Votes.query.filter_by(post_id=post_id, vote_type="downvote").count()
-
-            post.votes = upvote_count - downvote_count
-            db.session.commit()
-            return jsonify({'message': 'Vote removed successfully', 'votes': post.votes})
-        else:
-            return jsonify({'error': 'No vote found to remove'}), 404
-    else:
-        return jsonify({'error': 'Post not found'}), 404
-    
-@app.route('/check_vote/<int:post_id>/<vote_type>', methods=['GET'])
-def check_vote(post_id, vote_type):
-  user_id = current_user.id
-  vote_exists = Votes.query.filter_by(user_id=user_id, post_id=post_id, vote_type=vote_type).first()
-  return jsonify({'voted': vote_exists is not None})
 
 # #how far back was a post posted
 
